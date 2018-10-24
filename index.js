e067edf3299ffedae5f1c6d9d79930ebf60aa0f8
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-10-23 20:22:31 -0700

    define javascript variables

---
+++ index.js
@@ -0,0 +1,16 @@
+/*************
+testimonial section
+**************/
+
+//three dots
+const nextNext = document.getElementById('next-next')
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
+
+//individual testimonials
+const testi1 = document.querySelector('.testi1');
+const testi2 = document.querySelector('.testi2');
+const testi3 = document.querySelector('.testi3')
+
+//testimonial parent container
+const tests = document.querySelector('.tests');
